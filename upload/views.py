e13f03c17cec69ac75fd81bb534239e241eadd7e
mellisor <evanmellisor@gmail.com>
2019-08-10 18:43:25 -0400

    Added login validation

--- upload/views.py
+++ upload/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def login(request):
+    errors = {}
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user is not None:
             auth_login(request, user)
-        return redirect(reverse('upload:index'))
-    return render(request, 'upload/login.html')
+            return redirect(reverse('upload:index'))
+        else:
+                errors['error'] = 'Incorrect username or password'
+    return render(request, 'upload/login.html',context=errors)
 
 
+@login_required
 def index(request):
     return render(request, 'upload/index.html')
